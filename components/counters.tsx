e54984715d99d0821bf078538ee343e9331608da
Ajay-Dhangar <ajaydhangar49@gmail.com>
2023-12-25 12:26:48 +0530

    get started Js Mastery

--- components/counters.tsx
+++ components/counters.tsx
@@ -1,5 +1,3 @@
-// Example from https://beta.reactjs.org/learn
-
 import { useState } from 'react'
 import styles from './counters.module.css'
 
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>JavaScript Mastery</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Ajay-Dhangar/documentation',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Ajay-Dhangar/documentation',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Created by Ajay Dhangar with ❤️',
   },
 }
 
